4b63fbca95b746d3bef5bf5f3ac2278f39fcf77c
Hacknology <kolon01fantastik@gmail.com>
2017-01-30 20:19:47 +0300

    Add files via upload

---
+++ hostscan.py
@@ -0,0 +1,17 @@
+from scapy.all import * 
+import sys
+import netifaces
+
+Gw = netifaces.gateways()["default"][2]
+print(Gw)
+
+conf.verb = 0
+
+
+ip = "192.168.0."
+
+for i in range (1,254):
+    ipadres = ip + str(i)
+    ans=srp1(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ipadres),timeout=1,retry=1)
+    if ans:
+        print("IP adres: " + ans.psrc + " MAC : " + ans.hwsrc) 
